17c052a11b083e6efbdd6f8b3f9239cbb633cdc0
Romain Dejean <romain.dejean@ulb.be>
2025-11-12 13:25:47 +0100

    Ajout commentaires

--- lecture_pin.cpp
+++ lecture_pin.cpp
@@ -4,8 +4,9 @@
 #include <cstring>
 using namespace std ;
 
-
-void reverse(char mot[]) {
+//	Fonction permettant d'inverser les octets lu dans le fichier pin 
+//	Cela permet de régler les erreurs d'endianess
+void reverse(char mot[]) { 
     char inter = mot[0];
         mot[0] = mot[3];
         mot[3] = inter;
@@ -18,31 +19,42 @@ void reverse(char mot[]) {
 
 int lect_pin(string file_path, int indice) {
 	
+	//Initialisations
 	const string ext_pin = ".pin";
 	int val;
-	int longueur_titre =100;
+	int longueur_titre =100; //Initalisation de longueur titre à une grande valeur 
 	char reversed_bytes[4];
+	
+	//Ouverture du fichier pin
 	ifstream bdd_pin (file_path + ext_pin, ios::binary);
 	
-	
+	//Vérification de l'ouverture
 	if (!bdd_pin.is_open()) 
 		cerr << "Impossible d'ouvrir"<< endl;
-		
+	
+	// Boucle qui itère jusqu'à l'indice où se trouve les informations 
+	// de notre séquence dans le fichier phr
 	for (int i = 0; i<=indice+6;i++) {
 	bdd_pin.read(reversed_bytes, 4);
 	reverse(reversed_bytes);
 	memcpy(&val, reversed_bytes, 4);
-
+	
+	// Etant donné que le fichier pin contient des chaine de caractères
+	// à longueur inconnue, il faut incrémenter l'indice de manière à 
+	// prendre en compte ces chaines de caractères dans la lecture
 		if (i == 2) {
-			longueur_titre = val/4 ;
+			longueur_titre = val/4 ; 
 			indice += longueur_titre;
 			}
 			
-		if (i == 2 + longueur_titre + 1 ) {
+		if (i == 2 + longueur_titre + 1 ) {  	
 			int longueur_timestamp = val/4 ;
 			indice += longueur_timestamp;
 			}
 		}
+		
+	//Fermeture du fichier 
 	bdd_pin.close();
-	return val;
+	return val; //Retourne la position des informations de la séquence 
+				// dans le fichier phr
 }
--- lecture_psq.cpp
+++ lecture_psq.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 using namespace std ;
 
-
-char decoder(int nbr) {
+//Permet de convertir les octets en acides aminés selon les normes indiqués dans le fichier Far[10]
+char decoder(int nbr) { 
 	static const char table[28] = {
     '-','A','B','C','D','E','F','G','H','I','K','L','M','N','P','Q', 
     'R','S','T','V','W','X','Y','Z','U','*','O','J'};
@@ -14,39 +14,43 @@ char decoder(int nbr) {
 	
 	else {
 		cerr << "Erreur" << endl;
-		return '~';
+		return '~'; //Permet d'éviter d'avoir un warning au moment de la compilation
 	}
 	
 }	
 
 int lect_psq(string file_path, string seq_requete) {
+	
+	//Initialisations
 	const string ext_psq = ".psq";
 	int value;
-	char seq[1];
+	char seq;
 	string prot_complete = "";
-	int indice=0 ;
+	int indice=0 ; // Position de la séquence dans la bdd
 	
 	
-
+	// Ouverture du fichier psq
 	ifstream bdd_psq (file_path + ext_psq, ios::binary);
 	
+	// Vérification de l'ouverture
 	if (!bdd_psq.is_open()) 
 		cerr << "Impossible d'ouvrir" << endl;
 		
-	while (bdd_psq.read(seq, 1)) {
+	// Lire le fichier jusqu'à trouver la séquence de requête dans la base de données 
+	while (bdd_psq.read(&seq, 1)) {
 		value =0;
-		memcpy(&value, seq, 1);
+		memcpy(&value, &seq, 1);
 		if (value != 0) {
 			prot_complete += decoder(value) ;
 		}
 		else {
-			indice++;
+			indice++; // Incrémentation de l'indice à chaque itération
 			if (seq_requete == prot_complete) {
-				return indice;
+				return indice;	// Retourne le bonne indice
 			}
-			prot_complete = "";
+			prot_complete = ""; // Réinitialise la séquence à chaque itération
 		}
 	}
 	bdd_psq.close();
-	return 1;
+	return 1; // La séquence n'a pas été trouvée
 } 
